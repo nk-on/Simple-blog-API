dd9686b4009d2784914251ed1508c0b283f19e01
nk-on <2nkoz@proton.me>
2025-08-29 22:10:24 +0400

    Feat:sending message in case user requests non-existing author

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -6,6 +6,9 @@ function getPostById(req,res){
     const id = Number(req.params.id);
     const posts = db.posts;
     const post = posts.find(element => element.id === id);
+    if(!post){
+        res.json({message:'Error'})
+    }
     return res.json(post);
 }
 module.exports = {getAllPosts,getPostById}
