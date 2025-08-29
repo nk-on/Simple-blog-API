00bf4b0befde4b001a62c4f05a43bcc1dc80a220
nk-on <2nkoz@proton.me>
2025-08-29 22:21:34 +0400

    Creating separate route for posts

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const PORT = process.env.PORT;
 const express = require("express");
 const app = express();
 const {getAllPosts,getPostById }= require('./controllers/postControllers')
-app.get('/',getAllPosts);
+app.get('/',(req,res)=>{
+    return res.send('<h1>Welcome to Simple blog API</h1>');
+});
+app.get('/posts',getAllPosts);
 app.get('/posts/:id',getPostById);
 app.listen(PORT,()=>{
     console.log('i work')
